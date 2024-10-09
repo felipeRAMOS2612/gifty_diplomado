5651da16446284a16e2b5c2b040df5d6da912e97
Felipe Ramos <feliperamos2612@gmail.com>
2024-10-08 21:41:43 -0300

    modifique main

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def menu():
     print("1. Añadir producto")
     print("2. Añadir inventario")
     print("3. Exit")
-    option = int(input("Elige una opción: "))
-    if(option < 1 or option > 3 or option == None):
-        print("Opción no válida")
-        return menu()
-    return option
+    try:
+        option = int(input("Elige una opción: "))
+        return option
+    except ValueError:
+        print("Opción tiene que ser un numero")
 
 def add_product():
     name = input("Nombre del producto: ")
@@ -31,23 +31,18 @@ def add_inventory():
     inventory.save(name, ubication)
     print("Inventario añadido correctamente")
     
-def exit():
-    return False
-    
 def main():
-    option = menu()
-    if option == 1:
-        add_product()
-    elif option == 2:
-        add_inventory()
-    elif option == 3:
-        print("Hasta luego")
-        exit()
-    else:
-        print("Opción no válida")
-        main()
-
-
-while True:
-    if(not main()):
-        break
+    while True:
+        option = menu()
+        if option == 1:
+            add_product()
+        elif option == 2:
+            add_inventory()
+        elif option == 3:
+            print("Hasta luego")
+            break
+        else:
+            print("Opción no válida")
+            
+if __name__ == "__main__":
+    main()
